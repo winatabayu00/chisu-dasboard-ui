af739179414b63581a7c0e84b269b037a2dd0666
winatabayu <winatabayu01@gmail.com>
2024-10-01 21:48:17 +0700

    refactor: update header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ title }) => (
     <header className="d-flex justify-between items-center mb-6">
         <div className="d-flex bg-gray-100 p-4 flex justify-between items-center">
-            <h1 className="text-2xl font-bold text-gray-800" style={{paddingLeft:'280px'}}>{title}</h1>
+            <h1 className="text-2xl font-bold text-gray-800" style={{paddingLeft:'280px'}}>Dashboard ILP - Dinkes Kota Mojokerto</h1>
             <div className="flex items-center">
                 <div
                     className="bg-green-500 text-white rounded-full w-10 h-10 flex items-center justify-center mr-2">
